8903bef4e22a836c6af0c8fb04d5c7c882386276
Yenthe Oerlemans <yentheoerlemans@gmail.com>
2020-09-14 14:28:16 +0200

    renamed

--- main-application/src/app/app.module.ts
+++ main-application/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NormalComponent } from './normal-component';
+import { NormalComponent } from './normal.component';
 
 @NgModule({
   declarations: [
